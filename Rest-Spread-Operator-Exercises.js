a687961e19420abd3797e8cbce146b0019d1e3c1
mits <dmitrybaycharov@gmail.com>
2021-02-16 15:57:03 -0500

    finished

--- Rest-Spread-Operator-Exercises.js
+++ Rest-Spread-Operator-Exercises.js
@@ -10,10 +10,12 @@ function filterOutOdds() {
 }
 // Refactor it to use the rest operator & an arrow function:
 /* Write an ES2015 Version */
+const filterOutOdds = (...args) => args.filter(num=>num%2===0); 
+
 
 // findMin
 // Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
-
+const findMin = (...args) => Math.min(...args);
 // Make sure to do this using the rest and spread operator.
 
 findMin(1,4,12,-3) // -3
@@ -22,16 +24,20 @@ findMin(3,1) // 1
 
 // mergeObjects
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+const mergeObjects = (obj1,obj2) => ({...obj1,...obj2});
 
 mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
 // doubleAndReturnArgs
-// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. 
+//The function should return a new array with the original array values and all of additional arguments doubled.
+const doubleAndReturnArgs = (arr,...args) => [...arr,...args.map(val=>val*2)];
 
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 // Slice and Dice!
-// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
+// For this section, write the following functions using rest, spread and refactor these functions to be 
+//arrow functions!
 
 // Make sure that you are always returning a new array or object and not modifying the existing inputs.
 
@@ -39,39 +45,69 @@ doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 and return a new array without that item. */
 
 function removeRandom(items) {
-
+    const randomIndex = Math.floor(Math.random()*items.length);
+    const itemsCopy = [...items];
+    itemsCopy.splice(randomIndex,1);
+    return itemsCopy;
 }
+
+const removeRandom = items => {
+    const randomIndex = Math.floor(Math.random()*items.length);
+    const itemsCopy = [...items];
+    itemsCopy.splice(randomIndex,1);
+    return itemsCopy;
+}
+
 
 /** Return a new array with every item in array1 and array2. */
 
 function extend(array1, array2) {
-
+    return [...array1,...array2];
 }
+
+const extend = (array1,array2) => [...array1,...array2];
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 function addKeyVal(obj, key, val) {
-
+    return {...obj,...{key,val}};
 }
+
+const addKeyVal = (obj, key, val) => ({...obj,[key]:val});
 
 
 /** Return a new object with a key removed. */
 
 function removeKey(obj, key) {
-
+    let objCopy = {...obj};
+    delete objCopy[key];
+    return objCopy;  
 }
 
-
+const removeKey = (obj, key) => {
+    let objCopy = {...obj};
+    delete objCopy[key];
+    return objCopy;  
+}
 /** Combine two objects and return a new object. */
 
 function combine(obj1, obj2) {
-
+    return {...obj1,...obj2};
 }
 
+const combine = (obj1, obj2) => ({...obj1,...obj2});
 
 /** Return a new object with a modified key and value. */
 
 function update(obj, key, val) {
+    let objCopy = {...obj};
+    objCopy[key] = val;
+    return objCopy;
+}
 
+const update = (obj, key, val) => {
+    let objCopy = {...obj};
+    objCopy[key] = val;
+    return objCopy;
 }
